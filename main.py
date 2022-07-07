315636de4a1f4f79f59367eea9da0d254c002430
Khushboo Jayan <khushboojayan@gmail.com>
2022-07-07 01:49:59 +0100

    uploading an image

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 import pygame
+import os       #import operating systems to help you define the path for these images
 
 pygame.init()
-gameDisplay = pygame.display.set_mode((800,600))
+
+displayWidth = 800
+displayHeight = 600
+imgWidth = 100
+imgHeight = 100
+
+black = (0,0,0,)
+white = (255,255,255)
+
+
+gameDisplay = pygame.display.set_mode((displayWidth,displayHeight))
 pygame.display.set_caption('A bit racey')
 clock = pygame.time.Clock()
+carImg = pygame.image.load(os.path.join('Assets', 'raceCar.png'))
+raceCarImg = pygame.transform.rotate(pygame.transform.scale(carImg, (imgWidth,imgHeight)), -90)  #resize using scale rotate using rotate both in transform 
+
+
+#car function
+def car(x,y): 
+    gameDisplay.blit(raceCarImg,(x,y))
+
+x = (displayWidth * 0.45)
+y = (displayHeight * 0.8)
 
 crashed = False
 
@@ -13,8 +34,8 @@ while not crashed:
         if event.type == pygame.QUIT:
             crashed = True
 
-        print(event)
-
+    gameDisplay.fill(white)
+    car(x,y)
     pygame.display.update()
     clock.tick(60)
 
